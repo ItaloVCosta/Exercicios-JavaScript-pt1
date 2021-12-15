d806c885ce4e1474e0efad8154a4d85256f7bfdf
Italo Costa <italo.costa99@gmail.com>
2021-12-15 12:31:33 -0300

    Exercicio 4

---
+++ Exercicio4.js
@@ -0,0 +1,6 @@
+const resultadoResto = (dividendo, divisor) => {
+    console.log(`Resultado do divisao ${Math.floor(dividendo/divisor)}`);
+    console.log(`Resto do divisao ${dividendo%divisor}`);
+
+};
+resultadoResto(5,2)
---
+++ Exercico5.js
@@ -0,0 +1,5 @@
+const paraReal = (x)=>{
+    console.log(`O valor em real eh: R$${x.tofixed(2)}`)
+}
+
+console.log(0.1+0.2);
