5031958bf30bde752a23742e09780734e7c67bae
Italo Costa <italo.costa99@gmail.com>
2021-12-15 11:17:51 -0300

    Exercicio 1

---
+++ Exercicio1.js
@@ -0,0 +1,8 @@
+const operacos = (a,b) => {
+    console.log(`Soma:${a+b}`);
+    console.log(`Subtração:${a-b}`);
+    console.log(`Multiplicação:${a*b}`);
+    console.log(`Soma:${a/b}`);
+};
+
+operacos(2,2);
